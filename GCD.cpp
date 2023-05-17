c54981b639bf8128a1011a1bfd00f383ff6314f8
Sajjad Ranjbar <sajjadranjbaryazdi@gmail.com>
2023-05-17 12:57:09 +0330

    Update GCD.cpp

--- GCD.cpp
+++ GCD.cpp
@@ -7,7 +7,8 @@ int gcd(int a ,int b)
  int main()
 {
     int x,y;
-    cin>>x>>y;
+    x = 5;
+    y = 58;
     cout<<gcd(x,y)<<endl;   // gives -4
     cout<<gcd(abs(x),abs(y))<<endl;   //gives 4
     return 0;
